small cleanup of laws of motion and stages; derivatives of hetoutputs are incorrect

src/sequence_jacobian/blocks/support/law_of_motion.py
@@ -2,7 +2,6 @@ import numpy as np
 from . import het_compiled
 from ...utilities.interpolate import interpolate_coord_robust, interpolate_coord
 from ...utilities.multidim import batch_multiply_ith_dimension, multiply_ith_dimension
-from typing import Optional, Sequence, Any, List, Tuple, Union
 import copy
 
 class LawOfMotion:
@@ -16,7 +15,47 @@ class LawOfMotion:
     @property
     def T(self):
         pass
+    
 
+class Markov(LawOfMotion):
+    def __init__(self, i, Pi):
+        self.i = i      # axis of state that gets updated
+        self.Pi = Pi    # transition matrix Pi(s_i, s_i')
+
+    @property
+    def T(self):
+        newself = copy.copy(self)
+        newself.Pi = newself.Pi.T
+        if isinstance(newself.Pi, np.ndarray):
+            # optimizing: copy to get right order in memory
+            newself.Pi = newself.Pi.copy()
+        return newself
+
+    def __matmul__(self, X):
+        return multiply_ith_dimension(self.Pi, self.i, X)
+
+
+class DiscreteChoice(LawOfMotion):
+    def __init__(self, i, P):
+        self.i = i      # axis of state that gets updated
+        self.P = P      # choice prob P(d|...s_i...), 0 for unavailable choices
+
+        # cache "transposed" version of this, since we'll always need both
+        self.forward = True
+        self.P_T = P.swapaxes(0, 1+self.i).copy()
+
+    @property
+    def T(self):
+        newself = copy.copy(self)
+        newself.forward = not self.forward
+        return newself
+
+    def __matmul__(self, X):
+        if self.forward:
+            return batch_multiply_ith_dimension(self.P, self.i, X)
+        else:
+            return batch_multiply_ith_dimension(self.P_T, self.i, X)
+    
 
 def lottery_1d(a, a_grid, monotonic=False):
     if not monotonic:
@@ -26,12 +65,11 @@ def lottery_1d(a, a_grid, monotonic=False):
 
 
 class PolicyLottery1D(LawOfMotion):
-    # TODO: always operates on final dimension, make more general!
+    """Assumed to act on last axis."""
     def __init__(self, i, pi, grid, forward=True):
         # flatten non-policy dimensions into one because that's what methods accept
         self.i = i.reshape((-1,) + grid.shape)
         self.flatshape = self.i.shape
-
         self.pi = pi.reshape(self.flatshape)
 
         # but store original shape so we can convert all outputs to it
@@ -71,7 +109,7 @@ def lottery_2d(a, b, a_grid, b_grid, monotonic=False):
     if monotonic:
         # right now we have no monotonic 2D examples, so this shouldn't be called
         return PolicyLottery2D(*interpolate_coord(a_grid, a),
-                           *interpolate_coord(b_grid, b), a_grid, b_grid)
+                               *interpolate_coord(b_grid, b), a_grid, b_grid)
 
 
 class PolicyLottery2D(LawOfMotion):
@@ -115,44 +153,3 @@ class ShockedPolicyLottery2D(PolicyLottery2D):
             return het_compiled.forward_policy_shock_2d(X.reshape(self.flatshape), self.i, self.pi).reshape(self.shape)
         else:
             raise NotImplementedError
-
-
-class Markov(LawOfMotion):
-    def __init__(self, Pi, i):
-        self.Pi = Pi
-        self.i = i
-
-    @property
-    def T(self):
-        newself = copy.copy(self)
-        newself.Pi = newself.Pi.T
-        if isinstance(newself.Pi, np.ndarray):
-            # optimizing: copy to get right order in memory
-            newself.Pi = newself.Pi.copy()
-        return newself
-
-    def __matmul__(self, X):
-        return multiply_ith_dimension(self.Pi, self.i, X)
-
-
-class DiscreteChoice(LawOfMotion):
-    def __init__(self, P, i):
-        self.P = P                     # choice prob P(d|...s_i...), 0 for unavailable choices
-        self.i = i                     # dimension of state space that will be updated
-
-        # cache "transposed" version of this, since we'll always need both!
-        self.forward = True
-        self.P_T = P.swapaxes(0, 1+self.i).copy()
-
-    @property
-    def T(self):
-        newself = copy.copy(self)
-        newself.forward = not self.forward
-        return newself
-
-    def __matmul__(self, X):
-        if self.forward:
-            return batch_multiply_ith_dimension(self.P, self.i, X)
-        else:
-            return batch_multiply_ith_dimension(self.P_T, self.i, X)
-    

src/sequence_jacobian/blocks/support/stages.py
@@ -32,9 +32,7 @@ class Stage:
 
         if hetoutputs and self.hetoutputs is not None:
             inputs = {**inputs, **outputs}
-            #report.update(self.hetoutputs(all_inputs))
-            # for some reason self.hetoutputs returns its inputs too, need to fix that
-            report.update({k: v for k, v in self.hetoutputs(inputs).items() if k in self.hetoutputs.outputs})
+            report.update(self.hetoutputs(inputs, self.hetoutputs.outputs))
 
         if lawofmotion:
             return (backward_outputs, report), lom
@@ -42,11 +40,6 @@ class Stage:
             return backward_outputs, report
 
     def __init__(self, hetoutputs=None):
-        # instance variables of a stage:
-        # self.name = ""
-        # self.backward_outputs = OrderedSet([])
-        # self.report = OrderedSet([])
-        # self.inputs = OrderedSet([])
 
         self.original_inputs = self.inputs.copy()
         self.original_report = self.report.copy()
@@ -199,13 +192,14 @@ class Exogenous(Stage):
         self.report = OrderedSet([])
         self.inputs = backward | [markov_name]
 
+        # process hetoutputs, if any
         super().__init__(hetoutputs)
 
     def __repr__(self):
         return f"<Stage-Exogenous '{self.name}' with Markov matrix '{self.markov_name}'>"
     
     def backward_step(self, inputs, lawofmotion=False):
-        Pi = Markov(inputs[self.markov_name], self.index)
+        Pi = Markov(self.index, inputs[self.markov_name])
         outputs = {k: Pi @ inputs[k] for k in self.backward_outputs}
 
         if not lawofmotion:
@@ -214,11 +208,13 @@ class Exogenous(Stage):
             return outputs, Pi.T
     
     def backward_step_shock(self, ss, shocks, precomputed=None):
-        Pi = Markov(ss[self.markov_name], self.index)
-        outputs = {k: Pi @ shocks[k] for k in self.backward_outputs if k in shocks}
+        # first term: Pi_ss @ dv_{j+1}
+        Pi_ss = Markov(self.index, ss[self.markov_name])
+        outputs = {k: Pi_ss @ shocks[k] for k in self.backward_outputs if k in shocks}
 
+        # second term: dPi @ v_{ss, j+1}
         if self.markov_name in shocks:
-            dPi = Markov(shocks[self.markov_name], self.index)
+            dPi = Markov(self.index, shocks[self.markov_name])
             for k in self.backward_outputs:
                 if k in outputs:
                     outputs[k] += dPi @ ss[k]
@@ -257,6 +253,7 @@ class LogitChoice(Stage):
         if f is not None:
             self.inputs |= f.inputs
 
+        # process hetoutputs, if any
         super().__init__(hetoutputs)
 
     def __repr__(self):
@@ -266,7 +263,7 @@ class LogitChoice(Stage):
         # start with value we're given
         V_next = inputs[self.value]
 
-        # add dimension at beginning to allow for choice, then swap (today's choice determines next stages's state)
+        # add dimension at beginning to allow for choice, then swap (today's choice determines next stage's state)
         V = V_next[np.newaxis, ...]
         V = np.swapaxes(V, 0, self.index+1)
 
@@ -274,18 +271,13 @@ class LogitChoice(Stage):
         if self.f is not None:
             flow_u = self.f(inputs)
             flow_u = next(iter(flow_u.values()))
-        else:
-            # create phantom state variable, convenient but bit wasteful
-            nchoice = V.shape[0]
-            flow_u = np.zeros((nchoice,) + V_next.shape)
-
-        V = flow_u + V
+            V = flow_u + V
         
         # calculate choice probabilities and expected value
         P, EV = logit_choice(V, inputs[self.taste_shock_scale])
         
         # make law of motion, use it to take expectations of everything else
-        lom = DiscreteChoice(P, self.index)
+        lom = DiscreteChoice(self.index, P)
 
         # take expectations
         outputs = {k: lom.T @ inputs[k] for k in self.backward}
@@ -308,11 +300,8 @@ class LogitChoice(Stage):
         if f is not None:
             dflow_u = f.diff(shocks)
             dflow_u = next(iter(dflow_u.values()))
-            dflow_u = np.nan_to_num(dflow_u)  # -inf - (-inf) = nan, want zeros
-        else:
-            dflow_u = np.zeros_like(lom.P)
-        
-        dV = dflow_u + dV
+            dflow_u = np.nan_to_num(dflow_u)         # -inf - (-inf) = nan, want zeros
+            dV = dflow_u + dV
 
         # simply take expectations to get shock to expected value function (envelope result)
         dEV = np.sum(lom.P * dV, axis=0)
@@ -320,7 +309,7 @@ class LogitChoice(Stage):
         # calculate shocks to choice probabilities (note nifty broadcasting of dEV)
         scale = ss[self.taste_shock_scale]
         dP = lom.P * (dV - dEV) / scale
-        dlom = DiscreteChoice(dP, self.index)
+        dlom = DiscreteChoice(self.index, dP)
 
         # find shocks to outputs, aggregate everything of interest
         doutputs = {self.value: dEV}